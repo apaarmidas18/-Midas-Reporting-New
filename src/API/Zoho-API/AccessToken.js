a4d54708deec6fa59facc41aa41bfdd1674c3579
anubhav-midas <124804023+anubhav-midas@users.noreply.github.com>
2023-08-07 21:04:23 +0530

    async

--- src/API/Zoho-API/AccessToken.js
+++ src/API/Zoho-API/AccessToken.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 import { dhost } from "../../static";
 
-const AccessToken = () => {
+const AccessToken = async () => {
   const options = {
     method: "GET",
     redirect: "follow",
   };
   fetch(`${dhost}document/generateToken`, options)
     .then((response) => response.json())
-    .then((result) => {
+    .then(async (result) => {
       console.log(result.response);
-      const access_token = result.response.access_token;
-      localStorage.setItem("token", access_token);
+      const access_token = await result.response.access_token;
+      await localStorage.setItem("token", access_token);
     })
     .catch((error) => console.log(error));
 };
