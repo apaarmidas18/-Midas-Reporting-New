a19451b6b61a7e0ad621a38f6d48fb5d43ab0ba8
Apaar Gupta <apaar.gupta@midastravel.org>
2023-10-13 01:44:11 +0530

    Added New URL

--- src/static.js
+++ src/static.js
@@ -1,5 +1,5 @@
 // export const host = "http://192.168.1.42:8090/";
-export const host = "https://api.midascrm.tech/";
+export const host = "https://api.midastech.org/";
 // export const vercelHost = "http://192.168.1.250:9000/";
 export const vercelHost = "https://api-midas-legal-com.vercel.app/";
 export const nodeHost = "https://checklist.midascrm.tech/";
